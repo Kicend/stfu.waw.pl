5a07c227d5176ebe28cb128e7d15494249c3a5c9
Kicend <kicend@gmail.com>
2021-03-29 19:37:23 +0200

    Usunięcie pliku

--- static/js/slots.js
+++
@@ -1,9 +0,0 @@
-var socket = getSocketInstance();
-
-function takeSlot(slotId) {
-    socket.emit("request_username");
-    socket.once("get_username", function(msg) {
-        username = msg["username"];
-    });
-
-};
