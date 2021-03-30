0b729bb3f2a3af5bf95b637ec94d5997dbb27d89
Kicend <kicend@gmail.com>
2021-03-30 23:52:09 +0200

    Dostosowanie kodu do użytku strict mode

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,3 +1,4 @@
+"use strict";
 var socket = io.connect("http://127.0.0.1:5000");
 
 function getSocketInstance() {
--- static/js/status.js
+++ static/js/status.js
@@ -1,3 +1,4 @@
+"use strict";
 var socket = getSocketInstance();
 
 socket.on("online_users_list", function(msg) {
