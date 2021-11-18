e929ebb1b413240d31bf00a9284d5c1e73ffe2e5
Kicend <kicend@gmail.com>
2021-11-18 16:43:25 +0100

    Add decorator for check admin privileges

--- ext/decorators.py
+++ ext/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, render_template
 from flask_login import current_user
 
 
@@ -9,6 +9,16 @@ def check_confirmed(func):
         if current_user.confirmed is False:
             flash("Proszę potwiedź swój adres email", "warning")
             return redirect(url_for("routes.unconfirmed"))
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+
+def admin_privileges(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.admin is False:
+            return render_template("admin_privileges.html")
         return func(*args, **kwargs)
 
     return decorated_function
