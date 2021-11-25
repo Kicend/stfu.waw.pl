24a6f025da91098cb86550f20e1926b3c7d306ba
Kicend <kicend@gmail.com>
2021-11-25 16:42:11 +0100

    Add jail functionality

--- netopol/netopol.py
+++ netopol/netopol.py
@@ -1,5 +1,5 @@
 from json import load
-from random import randrange, choice
+from random import randrange, choice, shuffle
 from time import time
 
 class Session:
@@ -36,6 +36,8 @@ class Netopol(Session):
         self.properties_data = self.load_properties()
         self.start_balance = 1500
         self.accounts = dict.fromkeys([i for i in range(1, 11)], self.start_balance)
+        self.events = self.load_events()
+        self.events_cards_stack = [i for i in range(0, 3)]
         self.player_turn = None
         self.player_turn_state = None
         self.auction_state = False
@@ -48,6 +50,11 @@ class Netopol(Session):
     @staticmethod
     def load_properties():
         with open("netopol/data/properties.json", "r", encoding="utf-8") as f:
+            return load(f)
+
+    @staticmethod
+    def load_events():
+        with open("netopol/data/events.json", "r", encoding="utf-8") as f:
             return load(f)
 
     def get_coordinates(self):
@@ -70,6 +77,13 @@ class Netopol(Session):
         else:
             return False
 
+    # TODO: Dokończyć
+    def is_player_pass_start(self, player: Player):
+        if player.coordinates == "#0":
+            return True
+        elif player.last_coordinates:
+            pass
+
     @staticmethod
     def roll():
         dice_1 = randrange(1, 7)
@@ -87,6 +101,16 @@ class Netopol(Session):
             player.doublet = dices[1]
             if player.doublet:
                 player.doublet_counter += 1
+                if player.doublet_counter == 3:
+                    player.in_jail = True
+                    player.coordinates = "#10"
+            else:
+                player.doublet_counter = 0
+
+            if player.coordinates == "#30":
+                self.jail(player, 0)
+            elif player.coordinates in ("#2", "#7", "#17", "#22", "#33", "#36"):
+                self.fate(player)
         else:
             player.last_coordinates = player.coordinates
             player.coordinates = "#" + str(int(player.coordinates[1:]) + number)
@@ -98,6 +122,23 @@ class Netopol(Session):
             coordinates = str(int(player.coordinates[1:]) - 40)
             player.coordinates = "#" + coordinates
             self.accounts[player.seat] += 200
+
+    def jail(self, player: Player, mode: int = 0, sentence_length: int = 3):
+        if mode == 0:
+            player.in_jail = True
+            player.sentence_turn = sentence_length
+            player.coordinates = "#10"
+        else:
+            dices = self.roll()
+            player.doublet = dices[1]
+            if player.doublet:
+                player.in_jail = False
+                player.coordinates = "#" + str(int(player.coordinates[1:]) + dices[0])
+            else:
+                player.sentence_turn -= 1
+                if player.sentence_turn == 0:
+                    player.in_jail = False
+                    player.coordinates = "#" + str(int(player.coordinates[1:]) + dices[0])
 
     def buy(self, player: Player):
         property_card = self.properties_data[player.coordinates]
@@ -157,6 +198,19 @@ class Netopol(Session):
     def trade(self, player_1: int, player_2: int, player_1_items: dict, player_2_items: dict, result: bool):
         pass
 
+    def fate(self, player: Player):
+        event_card = self.events_cards_stack.pop(0)
+        self.events_cards_stack.append(event_card)
+        event_card = self.events["#" + str(event_card)]
+        if event_card["type"] == "teleport":
+            player.coordinates = event_card["coordinates"]
+        elif event_card["type"] == "income":
+            player_account = self.accounts[player.seat]
+            player_account += event_card["value"]
+        elif event_card["type"] == "pay":
+            player_account = self.accounts[player.seat]
+            player_account -= event_card["value"]
+
     def end_turn(self):
         current_player = self.player_turn
         current_player_index = self.active_players.index(current_player)
@@ -167,6 +221,7 @@ class Netopol(Session):
         self.player_turn_state = "roll"
 
     def start_game(self):
+        shuffle(self.events_cards_stack)
         for seat, player in self.players_seats.items():
             if player != "--":
                 self.active_players.append(Player(player, seat))
