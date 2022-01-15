c417e3c8325c47f92b42b0f62df633e2dd9ccd65
Kicend <kicend@gmail.com>
2022-01-15 22:09:29 +0100

    Move class Session to file

--- games/netopol/netopol.py
+++ games/netopol/netopol.py
@@ -1,21 +1,6 @@
 from json import load
 from random import randrange, choice, shuffle
-from time import time
-
-
-class Session:
-    def __init__(self, board_id, op, game_name):
-        self.board_id = board_id
-        self.op = op
-        self.game_name = game_name
-        self.created_at = time()
-        self.private = False
-        self.visible = False
-        self.max_slots = 2
-        self.players_seats = dict.fromkeys([i for i in range(1, 11)], "--")
-        self.players_list = []
-        self.active_players = []
-        self.state = "preparing"
+from games.base_classes.session import Session
 
 
 class Player:
