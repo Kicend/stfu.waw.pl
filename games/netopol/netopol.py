86c5a4485eb0625d7e33cb25d1576cbfe64d4d48
Kicend <kicend@gmail.com>
2022-02-04 12:25:47 +0100

    Assign to self.trade_recipient Player object instance instead only player nickname

--- games/netopol/netopol.py
+++ games/netopol/netopol.py
@@ -378,7 +378,7 @@ class Netopol(Session):
                     j += 1
 
             if i > 0 or j > 0:
-                self.trade_recipient = player_2.nickname
+                self.trade_recipient = player_2
                 self.trade_offer = {"player_1": player_1, "player_1_items": player_1_items, "player_2": player_2,
                                     "player_2_items": player_2_items}
                 self.journal_add_message(self.messages["send_offer_success"].format(player_one=player_1_id,
